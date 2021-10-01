b1ac0878a8fd44bbe5825e78f1cc2824a5511c40
Ethan Maxwell <e.maxwell18p@gmail.com>
2021-10-01 15:02:08 -0400

    Section 10 complete

---
+++ src/components/UI/Button/Input.js
@@ -0,0 +1,37 @@
+import { useRef, useImperativeHandle } from "react";
+import styles from "./input.module.css";
+
+const Input = React.forwardRef((props, ref) => {
+  const inputRef = useRef();
+
+  const activate = () => {
+    inputRef = useRef();
+  };
+
+  useImperativeHandle(ref, () => {
+    return {
+        focus: activate
+    };
+  });
+
+
+  return (
+    <div
+      className={`${styles.control} ${
+        props.isValid === false ? styles.invalid : ""
+      }`}
+    >
+      <label htmlFor="email">{props.label}</label>
+      <input
+        ref={inputRef}
+        type={props.type}
+        id={props.id}
+        value={emailState.value}
+        onChange={props.onChange}
+        onBlur={props.onBlur}
+      />
+    </div>
+  );
+});
+
+export default Input;
